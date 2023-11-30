efa6e4e3dcaa38252235e1f8987a1d08605aef20
GSherterton <gsherterton@gamil.com>
2023-11-30 14:35:03 -0300

    Atualizando o repositorio novamente

---
+++ BB/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    
+
+
+    return 0;
+}
