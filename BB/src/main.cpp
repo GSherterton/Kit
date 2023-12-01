afd3199824b44332200d00e1498ea4a14ba7b558
GSherterton <gsherterton@gamil.com>
2023-12-01 11:38:09 -0300

    Inicio do BB

--- BB/src/main.cpp
+++ BB/src/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-#include <algorithm>
-
 using namespace std;
 
-int main(){
-    
+#include "data.h"
+#include "hungarian.h"
 
+int main(int argc, char** argv) {
 
-    return 0;
+	Data * data = new Data(argc, argv[1]);
+	data->readData();
+
+	double **cost = new double*[data->getDimension()];
+	for (int i = 0; i < data->getDimension(); i++){
+		cost[i] = new double[data->getDimension()];
+		for (int j = 0; j < data->getDimension(); j++){
+			cost[i][j] = data->getDistance(i,j);
+		}
+	}
+
+	hungarian_problem_t p;
+	int mode = HUNGARIAN_MODE_MINIMIZE_COST;
+	hungarian_init(&p, cost, data->getDimension(), data->getDimension(), mode); // Carregando o problema
+
+	double obj_value = hungarian_solve(&p);
+	cout << "Obj. value: " << obj_value << endl;
+
+	cout << "Assignment" << endl;
+	hungarian_print_assignment(&p);
+
+	hungarian_free(&p);
+	for (int i = 0; i < data->getDimension(); i++) delete [] cost[i];
+	delete [] cost;
+	delete data;
+
+	return 0;
 }
