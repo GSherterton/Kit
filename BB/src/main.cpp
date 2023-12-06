71e8e9b7e1d3e216933f33fbafbcd7877b1a2a82
GSHerterton <gsherterton@gmail.com>
2023-12-06 11:06:25 -0300

    Salvando a versao com erro na hora de adicionar os nos

--- BB/src/main.cpp
+++ BB/src/main.cpp
@@ -25,6 +25,23 @@ void printarSubtour(vector<int>& subtour){
 	}cout << endl;
 }
 
+void printNode(Node node){
+	for(int i = 0; i < node.forbidden_arcs.size(); i++){
+		cout << node.forbidden_arcs[i].first << " - " << node.forbidden_arcs[i].second << endl;
+	}
+
+	cout << endl;
+
+	for(int i = 0; i < node.subtour.size(); i++){
+		printarSubtour(node.subtour[i]);
+	}
+
+	cout << endl << "LB: " << node.lower_bound << endl;
+	cout << "Index: " << node.chosen + 1;
+
+	cout << (node.feasible ? " | " : " | not ") << "feasible" << endl;
+}
+
 void preencherSubtour(hungarian_problem_t& p, vector<vector<int>>& subtour, int dimension){
 	int i, index;
 	vector<bool> passou(dimension, 0);
@@ -73,42 +90,113 @@ void preencherSubtour(hungarian_problem_t& p, vector<vector<int>>& subtour, int 
 }
 
 int menorSubtourIndex(vector<vector<int>>& subtour){
-	int menor = subtour[0].size();
+	pair<int, int> menor;//o primeiro vai ser o valor do menor e o segundo vai ser o index
+	menor.first = subtour[0].size();
+	menor.second = 0;
+
+	//cout << "Primeiro menor: " << menor.first << endl;
 
 	for(int i = 1; i < subtour.size(); i++){
-		if(subtour[i].size() < menor){
-			menor = subtour[i].size();
+		if(subtour[i].size() < menor.first){
+			menor.first = subtour[i].size();
+			menor.second = i;
+			//cout << "Atualizei o menor para " << menor.first << endl;
 		}
 	}
 
-	return menor;
+	return menor.second;
 }
 
-void iniciarNo(Node node, hungarian_problem_t& p, int dimension){
+void iniciarRaiz(Node node, hungarian_problem_t& p, int dimension){//talvez tirar isso
 	node.lower_bound = hungarian_solve(&p);
 	preencherSubtour(p, node.subtour, dimension);
 
 	node.chosen = menorSubtourIndex(node.subtour);
 	
-	if(node.subtour.size() > 1){
-		node.feasible = 0;
-	}else{
+	if(node.subtour.size() == 1){
 		node.feasible = 1;
+	}else{
+		node.feasible = 0;
 	}
 }
 
-void branchAndBound(hungarian_problem_t& p, int dimension){
+list<Node>::iterator branchingStrategy(list<Node> tree){//fazer por profundidade
+//Node* branchingStrategy(list<Node> tree){//fazer por profundidade
+	//a principio, so para a primeira iteracao
+	//Node* p = tree.begin();
+	//return p;
+	return tree.begin();
+}
+
+void proibindoArcos(hungarian_problem_t& p, Node& node){
+	for(int i = 0; i < node.forbidden_arcs.size(); i++){
+		p.cost[node.forbidden_arcs[i].first][node.forbidden_arcs[i].second] = 99999999;
+	}
+}	
+
+void getSolutionHungarian(Node& node, hungarian_problem_t p, int dimension){
+	proibindoArcos(p, node);
+	
+	node.lower_bound = hungarian_solve(&p);
+	preencherSubtour(p, node.subtour, dimension);
+
+	node.chosen = menorSubtourIndex(node.subtour);
+	
+	if(node.subtour.size() == 1){
+		node.feasible = 1;
+	}else{
+		node.feasible = 0;
+	}
+}
+
+void branchAndBound(hungarian_problem_t& p, int dimension, Data& data){
 	Node root;
-	iniciarNo(root, p, dimension);//fiz so ate a parte de achar os subtours e preencher o primeiro node
+	//iniciarRaiz(root, p, dimension);
 
 	list<Node> tree;
 	tree.push_back(root);
 
 	double upper_bound = numeric_limits<double>::infinity();
 
-	/*while(!tree.empty()){
-		auto node = branchingStrategy();
-	}*/
+	while(!tree.empty()){
+		cout << "Entrei aqui\n";
+		auto node = branchingStrategy(tree); //node apontara para algum no da arvore
+		getSolutionHungarian(*node, p, dimension);
+
+		//printNode(*node);
+
+		if(node->lower_bound > upper_bound){
+			tree.erase(node);
+			cout << "No invalido\n";
+			continue;
+		}
+		
+		if(node->feasible){
+			cout << "Possivel solucao encontrada\n";
+			upper_bound = min(upper_bound, node->lower_bound);
+		}else{
+			/*Adicionando os filhos*/
+			for(int i = 0; i < node->subtour[node->chosen].size() - 1; i++){//iterar por todos os arcos do subtour escolhido
+			//for(int i = 0; i < 3 - 1; i++){//iterar por todos os arcos do subtour escolhido
+				printNode(*node);
+
+				cout << "No adicionado\n";
+				Node n;//criar no auxiliar
+
+				n.forbidden_arcs = node->forbidden_arcs;//herdar os arcos passados
+
+				pair<int, int> forbidden_arc = {//proibir um arco novo
+					node->subtour[node->chosen][i],
+					node->subtour[node->chosen][i+1]
+				};
+
+				n.forbidden_arcs.push_back(forbidden_arc);
+				tree.push_back(n);//inserir novos nos na arvore
+			}
+		}
+
+		tree.erase(node);
+	}
 }
 
 int main(int argc, char** argv){
@@ -134,7 +222,22 @@ int main(int argc, char** argv){
 	cout << "Assignment" << endl;
 	hungarian_print_assignment(&p);*/
 
-	branchAndBound(p, data->getDimension());
+	list<int> lista;
+
+	lista.push_back(1);
+	auto x = lista.begin();
+
+	for(int i = 2; i < 10; i++){
+		lista.push_back(i);
+	}
+
+	cout << "P: " << *x << endl;
+
+	for(auto it = lista.begin(); it != lista.end(); it++){
+		cout << *it << " ";
+	}cout << endl;
+
+	//branchAndBound(p, data->getDimension(), *data);
 
 	//vector<vector<int>> subtour;
 
